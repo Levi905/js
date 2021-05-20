4883694fa16dd207bf6c5d47584adfb68ad23298
Levi905 <507816093@qq.com>
2021-05-20 14:09:07 +0800

    ajax

---
+++ 数据交互/data.js
@@ -0,0 +1,123 @@
+/**
+ * 示例：演艺数据
+ */
+ var good1 = {
+    "id": 1,
+    "good_name": "【成都】陕西人艺《白鹿原》2021封箱告别之旅【复活经典 九维钜献】",
+    "img_src": "https://img.alicdn.com/bao/uploaded/https://img.alicdn.com/imgextra/i3/2251059038/O1CN01okZ2Mh2GdSJXNrYRN_!!2251059038.png_q60.jpg_.webp",
+    "play_time": "2021.08.06-08.07",
+    "place": "成都市 | 四川大剧院 大剧场",
+    "citys": [
+        "1",
+        "4",
+        "6",
+        "7",
+        "9",
+        "12"
+    ],
+    "type": 3,
+    "times": [
+        "2021-06-05 周六 14:00",
+        "2021-06-06 周日 14:00"
+    ],
+    "price": "380",
+    "details": "<div class=\"words\"><p></p><p><span style=\"color: #ff0000;\"><strong data-spm-anchor-id=\"a2oeg.project.detail-info.i2.753877262EVW0B\">携带身份证实名登记入场，一票一证，全程佩戴口罩，请勿摘除！</strong></span></p>\n        <p>&nbsp;</p>\n        <p><span style=\"color: #ff0000;\"><strong>购票须知</strong></span></p>\n        <p><span style=\"color: #ff0000;\"><strong>特别提示</strong></span></p>\n        <p data-spm-anchor-id=\"a2oeg.project.detail-info.i0.753877262EVW0B\">根据《网络交易管理办法》中关于：“其他根据商品性质并经消费者在购买时确认不宜退货的商品，不适用无理由退货。”的规定，鉴于文体演出票品特殊性，演出门票为联网售票，门票和座位都是唯一的，具有时效性、唯一性、数字化等特殊属性，<span style=\"background-color: #ffff00;\">一旦票品售出，不支持退换，丢失不补办，过期作废</span>，请您在购买时务必仔细核对您的订单信息并审慎下单。（因不可抗力因素导致演出取消或延期除外）。儿童一律凭票入场，楼座票不可进入池座，由于安保和版权的原因，大多数演出、展览及比赛场所禁止携带食品、饮料、专业摄录设备、打火机等物品，请您注意现场工作人员和广播的提示，予以配合。</p>\n        <p>&nbsp;</p>\n        <p><strong>如何取票</strong></p>\n        <p>正式开票后，请到本剧目所在演出场馆（四川大剧院）自助取票机，凭身份证取票。</p>\n        <p>工作日取票：周一至周五9：00-17 : 00</p>\n        <p>演出当晚取票：演出前1小时左右</p>\n        <p>身份证取票的观众，取票一定要用带上并使用下单身份证取票，否则将无法取票和不能入场观看，后果自负）</p>\n        <p><br>取票地址：四川大剧院（成都市青羊区人民中路1号附2号）。（天府广场，地铁1、2线天府广场站F出口）</p>\n        <p>本剧院暂不支持停车，建议乘坐公共交通前往,给您带来的不便，敬请谅解！</p>\n        <p>&nbsp;</p>\n        <p><strong>配送说明</strong></p>\n        <p>系统暂不支持配送，请到本剧目所在演出场馆（四川大剧院）自助取票机，凭下单取票码/二维码/身份证取票。</p>\n        <p>&nbsp;</p>\n        <p><strong>购票咨询</strong></p>\n        <p>四川大剧院（锦城艺术宫）统一咨询电话热线：<strong>028-86650834&nbsp;</strong>（周一至周五9：00-17 : 00）</p>\n        <p>&nbsp;</p>\n        <p><strong>发票说明</strong></p>\n        <p><span style=\"background-color: #ffff00;\">需要开具发票的购票客户，请您务必在演出结束前，最迟演出结束后1个工作日内，提供相关发票信息至电话客服为您登记。</span>演出结束后将统一由演出主办方单位开具增值税发票。演出结束后，恕不再登记开发票，后果自负。<br>四川大剧院渠道：028-86650834；<br>大麦网订票的请联系大麦网：10103721；<br>其他渠道订票的联系购票渠道登记。</p>\n        <p><img src=\"http://img.alicdn.com/imgextra/i2/2251059038/O1CN011SU7Bm2GdSJZws7NM_!!2251059038.jpg_q60.jpg_.webp\" data-spm-anchor-id=\"a2oeg.project.detail-info.i1.753877262EVW0B\"></p>\n        <p><img src=\"http://img.alicdn.com/imgextra/i1/2251059038/O1CN01pqz6692GdSJRI1WPq_!!2251059038.jpg_q60.jpg_.webp\"></p>\n        <p><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN012xqXhq2GdSJWJLgU2_!!2251059038.jpg_q60.jpg_.webp\"></p>\n        <p><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01qoBRHi2GdSJVZOhW1_!!2251059038.jpg_q60.jpg_.webp\"><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01opbOAK2GdSJVZTGWy_!!2251059038.jpg_q60.jpg_.webp\"><img src=\"http://img.alicdn.com/imgextra/i1/2251059038/O1CN01dHu5l02GdSJabsu4l_!!2251059038.jpg_q60.jpg_.webp\"><img src=\"http://img.alicdn.com/imgextra/i3/2251059038/O1CN01isjxRF2GdSJWZTadK_!!2251059038.jpg_q60.jpg_.webp\"></p>\n        <p></p></div>"
+}
+    var good2 ={
+  "id": 2,
+  "good_name": "【成都】2021《魔兽世界》音乐节 成都站",
+  "img_src": "https://img.alicdn.com/bao/uploaded/https://img.alicdn.com/imgextra/i4/2251059038/O1CN01sv9lUb2GdSJcuDil1_!!2251059038.jpg_q60.jpg_.webp",
+  "play_time": "2021.06.05-06.06",
+  "place": "成都市 | 东郊记忆",
+  "citys": [
+    "3",
+    "5",
+    "1",
+    "6"
+  ],
+  "type": 1,
+  "times": [
+    "2021-06-05 周六 14:00",
+    "2021-06-06 周日 14:00"
+  ],
+  "price": "380",
+  "details": "<div class=\"words\"><p><p><img src=\"http://img.alicdn.com/imgextra/i3/2251059038/O1CN015Ii1Mm2GdSJYxjQrR_!!2251059038.jpg_q60.jpg_.webp\" width=\"700\" height=\"6718\" damai_width=\"700\" damai_height=\"6718\"><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01b0cY3V2GdSJRltcrZ_!!2251059038.jpg_q60.jpg_.webp\" width=\"700\" height=\"7217\" damai_width=\"700\" damai_height=\"7217\"></p></p></div>"
+}
+    var good3 = {
+    "id": 10,
+    "good_name": "【深圳】IBEW始立 职业摔角live",
+    "img_src": "https://img.alicdn.com/bao/uploaded/i1/2251059038/O1CN010yFzEl2GdSJoGvgk8_!!0-item_pic.jpg_q60.jpg_.webp",
+    "play_time": "2021.05.25 周二 20:00",
+    "place": "深圳市 | 腾讯WeSpace ",
+    "citys": [
+        "11"
+    ],
+    "type": 4,
+    "times": [
+        "2021.05.25 周二 20:00"
+    ],
+    "price": [
+        "无座票100元",
+        "普通票200元",
+        "VIP票588元"
+    ],
+    "details": "<div class=\"words\"><p><p><strong data-spm-anchor-id=\"a2oeg.project.detail-info.i0.4ad844e2D6H9Pt\">IBEW x MKW 《始立 The Foundation》深圳首战</strong></p>\n<p><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01KI6Rdr2GdSJisRvFg_!!2251059038.png_q60.jpg_.webp\" width=\"514\" height=\"498\" damai_width=\"514\" damai_height=\"498\"></p>\n<p>什么是IBEW？</p>\n<p>(Infinity Battle Empire prowrestling)</p>\n<p>无限战斗帝国职业摔角</p>\n<p>国内首创融入现场/电视剧集形式，以摔角赛事为载体的娱乐体育品牌。</p>\n<p>&nbsp;</p>\n<p>IBEW摔角赛事将会以炫目的摔角技艺与真实的人物剧情作为核心,并且会定期在体育馆与剧场里进行赛事演出。</p>\n<p>&nbsp;</p>\n<p>最为精彩的现场赛事</p>\n<p>&nbsp;</p>\n<p>最具新意的娱乐剧情</p>\n<p>&nbsp;</p>\n<p>最优秀丰富的选手阵容</p>\n<p>&nbsp;</p>\n<p>最为精美的后期制作</p>\n<p>&nbsp;</p>\n<p>为您带来颠覆性、独特性</p>\n<p>&nbsp;</p>\n<p>你没有看过的中国职业摔角</p>\n<p>&nbsp;</p>\n<p>始立《The Foundation》</p>\n<p>IBEW扬旗首战</p>\n<p>联合中国摔角先锋联盟</p>\n<p>MKW摔角王国</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01UUwBaD2GdSJkouQby_!!2251059038.png_q60.jpg_.webp\" width=\"499\" height=\"499\" damai_width=\"499\" damai_height=\"499\"></p>\n<p>以及</p>\n<p>MMC战神录</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01rgHeF42GdSJplH7TE_!!2251059038.png_q60.jpg_.webp\" width=\"501\" height=\"500\" damai_width=\"501\" damai_height=\"500\"></p>\n<p>5月25日</p>\n<p>&nbsp;</p>\n<p>将于中国摔角文化盛地</p>\n<p>&nbsp;</p>\n<p>广东深圳</p>\n<p>为大家带来最精彩的现场赛事</p>\n<p>&nbsp;</p>\n<p>明星选手介绍</p>\n<p>&nbsp;</p>\n<p>Buffa &amp; Da Boombox</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i3/2251059038/O1CN01vxeSpI2GdSJkswZ9T_!!2251059038.png_q60.jpg_.webp\" width=\"538\" height=\"657\" damai_width=\"538\" damai_height=\"657\"></p>\n<p>做为前NWA冠军的布法 国内的摔迷也并不陌生 布法和他的大音响依旧会在比赛中给大家带来惊喜与快乐</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i3/2251059038/O1CN01vPJzct2GdSJrs9Scu_!!2251059038.png_q60.jpg_.webp\" width=\"568\" height=\"645\" damai_width=\"568\" damai_height=\"645\"></p>\n<p>这次我们的摔迷之友又会与谁在擂台上共舞呢 我们拭目以待吧!</p>\n<p>&nbsp;</p>\n<p>The Stable</p>\n<p>统治力骑士</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i2/2251059038/O1CN01tLeK6V2GdSJnTr1MB_!!2251059038.png_q60.jpg_.webp\" width=\"578\" height=\"321\" damai_width=\"578\" damai_height=\"321\"></p>\n<p>2017年，大咖AL带领谢未来、钱叔、大山姆、女皇玛丽组建了MKW第一支摔角军团统治力骑士，在大咖AL的领导下，该组合横扫中摔难逢敌手，无恶不作。在统治力骑士组合的协助下，大山姆使用卑劣手段赢得MKW世界冠军。也击败了来自世界各地的名将，连身高2米01，体重182KG的IMPACT选手“刚果金刚”都被斩落马下。</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i3/2251059038/O1CN01Ycvszk2GdSJgy3do5_!!2251059038.png_q60.jpg_.webp\" width=\"601\" height=\"400\" damai_width=\"601\" damai_height=\"400\"></p>\n<p><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01K6ReqR2GdSJiIxO5v_!!2251059038.png_q60.jpg_.webp\" width=\"599\" height=\"481\" damai_width=\"599\" damai_height=\"481\"></p>\n<p>但是在IBEW的擂台上，我们可以看到The Stable再次使出极度卑劣手段赢得比赛吗 ？还是最终邪不胜正被正派选手击败呢？</p>\n<p>&nbsp;</p>\n<p>僵尸龙</p>\n<p>Zombie Dragon</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i2/2251059038/O1CN01ffmF2o2GdSJm4Qv86_!!2251059038.png_q60.jpg_.webp\" width=\"589\" height=\"589\" damai_width=\"589\" damai_height=\"589\"></p>\n<p>出生地未知，身高体重不详的僵尸龙在2019年9月7日参加了MKW赛事《王国乱斗》，击败各路强敌取得优胜，向五次卫冕MKW世界冠军的苏家正发出挑战。</p>\n<p>最终僵尸龙极具争议的击败苏家正，结束了他181天的王座统治，成为第五代MKW世界冠军！僵尸龙夺冠后一直与大咖AL率领的统治力骑士博弈，在MKW周播系列赛事，他遭遇了夺冠后的首败。</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i1/2251059038/O1CN01a4V5bn2GdSJiJ2Hm3_!!2251059038.png_q60.jpg_.webp\" width=\"561\" height=\"373\" damai_width=\"561\" damai_height=\"373\"></p>\n<p>大咖AL一直以此为傲，最终也因为自己的傲慢付出了代价，僵尸龙最终把大咖AL淹没在火海当中，击败大山姆卫冕冠军，彻底挫败了统治力骑士。</p>\n<p>&nbsp;</p>\n<p>击溃统治力骑士之后，僵尸龙不断巩固自己的冠军统治，黑曼巴，凯姆弗格森，外教史蒂夫，DC陈，寒光，M.A，一位又一位的挑战者最终都以失败者的身份走出擂台。</p>\n<p>&nbsp;</p>\n<p>2019年12月14日—卫冕至今</p>\n<p>没有一个人能撼动僵尸龙的冠军统治，2021年依然是他的时代！</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01SHt3iA2GdSJrsBTXe_!!2251059038.png_q60.jpg_.webp\" width=\"569\" height=\"427\" damai_width=\"569\" damai_height=\"427\"></p>\n<p>特邀嘉宾</p>\n<p>&nbsp;</p>\n<p>WWE中国第一人王彬</p>\n<p>将来到现场！</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i1/2251059038/O1CN01aEl12r2GdSJcEbxKT_!!2251059038.png_q60.jpg_.webp\" width=\"569\" height=\"322\" damai_width=\"569\" damai_height=\"322\"></p>\n<p><img src=\"http://img.alicdn.com/imgextra/i4/2251059038/O1CN01clxxYE2GdSJkt1CNr_!!2251059038.png_q60.jpg_.webp\" width=\"571\" height=\"571\" damai_width=\"571\" damai_height=\"571\"></p>\n<p>更有神秘Rapper为大家带来中场演出！！！！！</p>\n<p>&nbsp;</p>\n<p>赛事地址</p>\n<p>广东省-深圳市-福田区-笋岗西路深业上城CEEC(Loft-D4)4楼 腾讯WESPACE</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i1/2251059038/O1CN011J7o7I2GdSJnTtdqL_!!2251059038.png_q60.jpg_.webp\" width=\"562\" height=\"316\" damai_width=\"562\" damai_height=\"316\"></p>\n<p><img src=\"http://img.alicdn.com/imgextra/i3/2251059038/O1CN01xYwW9d2GdSJkp3pBT_!!2251059038.png_q60.jpg_.webp\" width=\"563\" height=\"375\" damai_width=\"563\" damai_height=\"375\"></p>\n<p>5月25日 我们深圳不见不散！</p>\n<p><img src=\"http://img.alicdn.com/imgextra/i2/2251059038/O1CN01O8hL9z2GdSJnTuusz_!!2251059038.png_q60.jpg_.webp\" width=\"575\" height=\"847\" damai_width=\"575\" damai_height=\"847\"></p></p></div>"
+}   
+/**
+ * 示例：电影数据
+ */
+
+var movie1={
+    "id": "1292213",
+    "title": "大话西游之大圣娶亲 西遊記大結局之仙履奇緣",
+    "actors": "周星驰 / 吴孟达 / 朱茵",
+    "imgSrc": "https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2455050536.jpg",
+    "director": "刘镇伟",
+    "score": "9.2",
+    "release": "1995-02-04(中国香港) 2014-10-24(中国大陆) 2017-04-13(中国大陆重映) ",
+    "movieType": [
+      1,
+      8,
+      11,
+      3
+    ],
+    "language": " 粤语 / 汉语普通话",
+    "region": "中国香港 中国大陆",
+    "desc": "至尊宝（周星驰 饰）被月光宝盒带回到五百年前，遇见紫霞仙子（朱茵 饰），被对方打上烙印成为对方的人，并发觉自己已变成孙悟空。\n                                    \n  紫霞与青霞（朱茵 饰）本是如来佛祖座前日月神灯的灯芯（白天是紫霞，晚上是青霞），二人虽然同一肉身却仇恨颇深，因此紫霞立下誓言，谁能拔出她手中的紫青宝剑，谁就是她的意中人。紫青宝剑被至尊宝于不经意间拔出，紫霞决定以身相许，却遭一心记挂白晶晶（莫文蔚 饰）的至尊宝拒绝。后牛魔王救下迷失在沙漠中的紫霞，并逼紫霞与他成婚，关键时刻，至尊宝现身。",
+    "duration": "95分钟",
+    "posterSrc": "https://img3.doubanio.com/img/trailer/medium/2455167430.jpg?",
+    "trailerLink": "../../video/302150564.mp4",
+    "comments": [
+      "无论是看第三十遍，还是第三十一遍，我都会对着那个寂寥的背影流泪。那落寞的大圣身上烙印着我青春时所有梦想，我们曾以为自己会成为盖世英雄，以为自己是这个世界的唯一，以为爱情专为自己存在，可是渐渐发现自己和别人没有什么不同，当你踏上社会时，总会有人看着你的背影说：看，他好像条狗啊！",
+      "一生所爱：我的意中人是个盖世英雄，有一天他会穿着金盔金甲，踩着七色的云彩来娶我。我猜中了前头，可是我猜不着这结局……",
+      "坐我旁边的那个年近四十、西装革履的男人从电影刚开始就止不住眼泪，有些地方哭着笑出声。而他的年轻女伴从开头就一直低头玩手机、到经典场景就煞有介事地拍张照片、不到一半时一直嚷嚷着无聊，硬是把那个男人拉走了。",
+      "因为你还没有遇到给你三颗痣的人",
+      "请收藏粤语版。「2010」：1218，这么多年后重看，居然还是湿了。【2014】终于在大荧幕上看了。虽然是普通话。经得起这么多次的反刍，可以确认是经典了。想起东邪西毒重演时提前离场的对比，简直。"
+    ]
+}
+var movie2 ={
+    "id": "1295038",
+    "title": "哈利·波特与魔法石 Harry Potter and the Sorcerer's Stone",
+    "actors": "丹尼尔·雷德克里夫 / 艾玛·沃森 / 鲁伯特·格林特",
+    "imgSrc": "https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2614949805.jpg",
+    "director": "克里斯·哥伦布",
+    "score": "9.0",
+    "release": "2002-01-26(中国大陆) 2020-08-14(中国大陆重映) 2001-11-04(英国首映) 2001-11-16(美国) ",
+    "movieType": [
+      10,
+      11
+    ],
+    "language": " 英语",
+    "region": "美国 英国",
+    "desc": "哈利波特是一个孤儿，从小寄养在姨妈家，受尽欺凌。但就在哈利11岁生日的时候，他意外收到了霍格沃茨学院的入学通知书。哈利从该学院派来接他的巨人海格口中得知，这是一间魔法学院，并得知了自己的身世，原来哈利的父母都是伟大的魔法师，在对付伏地魔的战斗中双双献身，唯有哈利幸免于难。\n                                    \n                                　　哈利进入霍格沃茨后，表现出了超乎想象的飞行天赋，得到麦格教授的推荐进入了格兰芬多的魁地奇球队。另一方面，哈利发现霍格沃茨学院内有一股黑暗势力似乎在暗暗滋长，揭开谜团的关键就在有凶恶的三头犬守护的房间内。\n                                    \n                                　　哈利、罗恩和赫敏三个好朋友决定探个究竟。",
+    "duration": "152分钟",
+    "posterSrc": "https://img3.doubanio.com/img/trailer/medium/2617066051.jpg?1597302617",
+    "trailerLink": "../../video/402640693.mp4",
+    "comments": [
+      "花了8年才看完这部电影....",
+      "那时候是有多可爱啊，刚接触魔法的种种新鲜和刺激，比起后面的黑暗剧情以及丹尼尔越来越大叔的模样，真是怀念这里啊。",
+      "一个全新的世界被打开了，新奇而不可思议，我们都是麻瓜，幻想一天早上猫头鹰会送来霍格沃兹的入学通知书。铁三角组合很可爱。",
+      "第一部好看啊  哈哈",
+      "哈利波特系列的电影与小说，我能感受他们的喜怒哀乐，一同烦恼学生时代的“难熬”，当这一系列结束时，再回头看这第一部，有一种“曾当年”的感受，我私以为作者以后再写出什么样的故事也不能如当年那样打动人心，毕竟这已成了我心中的白月光，不是吗"
+    ]
+  }
